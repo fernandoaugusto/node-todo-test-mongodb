2a22dded9739252d23667839755d69b20f220dc1
Fernando Augusto <fernando@MacBook-Pro-de-Fernando.local>
2017-09-20 20:24:42 -0300

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({_id: ObjectId("59c2e8b4cb33e10da8ffab9f")}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find().toArray().then((users) => {
+    console.log(JSON.stringify(users, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch Users', err);
+  });
+
+  // db.close()
+});
