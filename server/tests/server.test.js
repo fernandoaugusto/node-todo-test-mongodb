4ab556e10261e3247b7e1ed255fa47bb60c7ff4c
Fernando Augusto <fernando@MacBook-Pro-de-Fernando.local>
2017-09-28 17:02:39 -0300

    Added PATCH/todos/:id and Server Tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -138,3 +138,36 @@ describe('DELETE /todos/:id', () => {
   });
 
 });
+
+describe('PATCH /todos/:id', () => {
+
+  it('should update the todo', (done) => {
+      var hexId = todos[0]._id.toHexString();
+      var text = 'Test Patch';
+      var completed = true;
+      request(app)
+        .patch(`/todos/${hexId}`)
+        .send({text, completed})
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(text);
+          expect(res.body.todo.completed).toBe(true);
+          expect(res.body.todo.completedAt).toBeA('number');
+        })
+        .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var completed = false;
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({completed})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.completedAt).toBe(null);
+      })
+      .end(done);
+  });
+
+});
